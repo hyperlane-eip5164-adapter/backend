adb487adce98e9a1c2de26695f89cfe5af120011
degencodebeast <degencodebeast@protonmail.com>
2023-08-11 12:34:51 +0100

    initialized project

---
+++ hardhat.config.ts
@@ -0,0 +1,70 @@
+// import { HardhatUserConfig, task } from "hardhat/config";
+// import "@nomicfoundation/hardhat-toolbox";
+
+// task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+//   const accounts = await hre.ethers.getSigners();
+
+//   for (const account of accounts) {
+//     console.log(account.address);
+//     console.log((await account.getBalance()).toString());
+//   }
+// });
+
+
+// const config: HardhatUserConfig = {
+//   paths: { tests: "tests" },
+//   networks: {hardhat: {hardfork: "merge"}},
+//   solidity: {
+//       version: "0.8.18",
+//       settings: {
+//         optimizer: {
+//           enabled: false,
+//           runs: 0,
+//         },
+//       },
+//     }
+// };
+
+// export default config;
+
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import 'hardhat-gas-reporter';
+import 'solidity-coverage';
+//import "@nomiclabs/hardhat-ethers";
+
+const config: HardhatUserConfig = {
+  // solidity: "0.8.0",
+  paths: { tests: "tests" },
+  solidity: {
+    version: '0.8.9',
+    settings: {
+      evmVersion: process.env.EVM_VERSION || 'london',
+      optimizer: {
+        enabled: true,
+        runs: 200,
+        details: {
+          peephole: true,
+          inliner: true,
+          jumpdestRemover: true,
+          orderLiterals: true,
+          deduplicate: true,
+          cse: true,
+          constantOptimizer: true,
+          yul: true,
+          yulDetails: {
+            stackAllocation: true,
+          },
+        },
+      },
+
+    },
+
+  },
+  typechain: {
+    target: "ethers-v5"
+  },
+  
+};
+
+export default config;
